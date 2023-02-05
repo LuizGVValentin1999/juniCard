c22fdf27ba83f0f848a05d5c8b49ca45e0f0d024
Luiz Valentin <luiz.gustavo1999@hotmail.com>
2023-02-05 21:38:42 +0000

    Update 4 files

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { Login } from './pages/Login'
-import { Saldo } from './pages/Saldo'
-import { Compra } from './pages/Compra'
-import { Lista } from './pages/Lista'
+import { Login } from "./pages/Login";
+import { Saldo } from "./pages/Saldo";
+import { Compra } from "./pages/Compra";
+import { Lista } from "./pages/Lista";
+import { QrCode } from "./pages/QrCode";
 
 export function Router() {
   return (
     <Routes>
-      <Route >
+      <Route>
         <Route path="/" element={<Login />} />
         <Route path="/Saldo" element={<Saldo />} />
         <Route path="/Compra" element={<Compra />} />
         <Route path="/Lista" element={<Lista />} />
+        <Route path="/QrCode" element={<QrCode />} />
       </Route>
     </Routes>
-  )
+  );
 }
