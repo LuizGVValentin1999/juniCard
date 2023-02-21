5e5c34bac1ea2adfe57f1f11728699e169cc3dd0
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2023-02-21 10:04:38 -0300

    saldo e trasferencia

--- src/pages/MeuQrcode/index.tsx
+++ src/pages/MeuQrcode/index.tsx
@@ -1,14 +1,47 @@
 
 
+import { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
+import { api } from "../../api/axios";
+import { useCookies } from "react-cookie";
+
+
+interface alunoProps {
+  ID: string[];
+  USUARIO: string[];
+  ADMIN: string[];
+  SALDO: string[];
+}
 
 export function MeuQrcode() {
+  const [infoAluno, setInfoAluno] = useState<alunoProps | null>(null);
+  const [cookies, setCookie] = useCookies();
+
+  
+  useEffect(() => {
+    meuQRcode();
+  }, [])
+
+   async function meuQRcode() {
+    try{
+      let response = await api.get('meusdados',{ headers: { Authorization: `Bearer ${cookies.tokenJunicard}` }});
+      if(response.data.USUARIO){
+        setInfoAluno(response.data);
+      }
+    }
+    catch (error:any) {
+      if(error.response.data.status === '0'){
+        setCookie('tokenJunicard', false);
+      }
+      alert(error.response.data.msg);
+    }
+   }
 
   return (
-    <div className=" centralizameuqrcode" >
-      <h1>Qr Code para tranferencia de Juniedas</h1>
+    <div className="centralizameuqrcode" >
+      <h1>Qr Code para tranferencia de Juniedas de {infoAluno?.USUARIO}</h1>
     <QRCode
-    value='batadgflçkgkdfmgkfdgjfndjngfjkdnjta'
+    value={JSON.stringify(infoAluno)}
     />
    </div>
   );
